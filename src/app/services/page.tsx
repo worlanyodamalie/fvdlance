56aa6ea5dba50bdb80e92e72d22bd2fe4a606de8
Worlanyo <worladamalie@gmail.com>
2025-02-24 06:50:58 +0000

    implement services page

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,5 +1,9 @@
 "use client"
 import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+import { usePathname } from 'next/navigation'
+
 
 export default function Home() {
   const featureCards = [
@@ -95,11 +99,155 @@ export default function Home() {
       title: "Lorem ipsum pativa"
     },
   ]
+  
+  const [isVisibleNav,setisVisibleNav] = useState(false)
 
+  const pathname = usePathname()
+
+  const getLinkClassName = (path: string) => {
+    return `relative ${
+      pathname === path 
+        ? 'text-[#A1B1FD]' 
+        : 'text-[#8BF645] hover:text-[#A1B1FD] transition-colors duration-300'
+    }`
+  }
+
+  const getUnderlineClassName = (path: string) => {
+    return pathname === path 
+      ? ''
+      : "relative before:content-[''] before:absolute before:bottom-0 before:left-0 before:w-full before:h-[3px] before:bg-current before:transition-transform before:duration-500 before:ease-in-out"
+  }
 
   return (
     <div className="body__wrapper">
-      <header></header>
+      <section className="block-hero relative h-screen pb-16">
+        <header className="header">
+          <div className="header__inner relative flex justify-between items-start w-full">
+            <Link href="/"></Link>
+            <div className="header__btns">
+              <button
+                className="header__btn cursor-pointer h-[40px] w-[40px]"
+                type="button"
+                onClick={() => setisVisibleNav(true)}
+              >
+                <span className="header__btn-inner">
+                  <svg className="header__btn-icon" viewBox="0 0 1024 1024">
+                    <g className="header__btn-line top">
+                      <g className="header__btn-line-inner header__btn-top left">
+                        <path d="M0 253.972h256v56.889h-256v-56.889z"></path>
+                      </g>
+                      <g className="header__btn-line-inner header__btn-top center">
+                        <path d="M256 225.528h56.889v56.889h-56.889v-56.889z"></path>
+                      </g>
+                      <g className="header__btn-line-inner header__btn-top right">
+                        <path d="M312.889 253.972h711.111v56.889h-711.111v-56.889z"></path>
+                      </g>
+                    </g>
+                    <g className="header__btn-line center">
+                      <g className="header__btn-line-inner header__btn-center left">
+                        <path d="M0 481.528h853.333v56.889h-853.333v-56.889z"></path>
+                      </g>
+                      <g className="header__btn-line-inner header__btn-center right">
+                        <path d="M853.333 424.639v56.889h170.667v-56.889h-170.667z"></path>
+                      </g>
+                    </g>
+                    <g className="header__btn-line bottom">
+                      <g className="header__btn-line-inner header__btn-bottom left">
+                        <path d="M0 741.584h85.333v56.889h-85.333v-56.889z"></path>
+                      </g>
+                      <g className="header__btn-line-inner header__btn-bottom center">
+                        <path d="M85.333 684.696h369.778v56.889h-369.778v-56.889z"></path>
+                      </g>
+                      <g className="header__btn-line-inner header__btn-bottom right">
+                        <path d="M1024 741.57h-568.889v56.889h568.889v-56.889z"></path>
+                      </g>
+                    </g>
+                  </svg>
+                </span>
+              </button>
+            </div>
+            <div className={`header-main ${isVisibleNav ? "visible-nav" : ""}`}>
+              <div
+                className={`absolute top-[101px] w-[50px] h-[101px] right-[70px] ${
+                  isVisibleNav ? "z-[1]" : ""
+                }`}
+              >
+                <button
+                  className="header__btn-close "
+                  type="button"
+                  onClick={() => setisVisibleNav(false)}
+                >
+                  <svg className="header__btn-close-icon cursor-pointer">
+                    <use xlinkHref="#icon-close">
+                      <symbol id="icon-close" viewBox="0 0 32 32">
+                        <path d="M1.403 30.506l23.472-23.472-1.494-1.494 5.54-5.54 1.494 1.494-5.54 5.54 1.494 1.494-23.472 23.472-1.494-1.494z"></path>
+                        <path d="M19.955 21.842l-18.679-18.679 1.494-1.494 18.679 18.679-1.494 1.494z"></path>
+                        <path d="M29.23 31.116l-7.781-7.781 1.494-1.494 7.781 7.781-1.494 1.494z"></path>
+                        <path d="M20.702 24.083l-1.494-1.494 1.494-1.494 1.494 1.494-1.494 1.494z"></path>
+                      </symbol>
+                    </use>
+                  </svg>
+                </button>
+              </div>
+              <div className="header-main--inner">
+                <div className="header-main__nav block-title-m font-bebas w-full">
+                  <p className="leading-none">
+                    Welcome to FVDLance <br /> You can checkout{" "}
+                    <Link
+                      className={`${getLinkClassName(
+                        "/services"
+                      )} ${getUnderlineClassName("/services")}`}
+                      href="/services"
+                    >
+                      our services
+                    </Link>
+                    ,{" "}
+                    <Link
+                      href="/work"
+                      className={`${getLinkClassName(
+                        "/work"
+                      )} ${getUnderlineClassName("/work")}`}
+                    >
+                      our work
+                    </Link>
+                    ,<br />
+                    <Link
+                      href="/jobs"
+                      className={`${getLinkClassName(
+                        "/jobs"
+                      )} ${getUnderlineClassName("/jobs")}`}
+                    >
+                      our job openings
+                    </Link>{" "}
+                    <span className="leading-[1.4]">or feel free to reach out</span>
+                    
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </header>
+        <div className="block-hero__inner w-full h-full flex justify-between items-end overflow-hidden ">
+          <h1 className="block-title block-hero--title font-bebas">
+            <span className="mb-[0.12em]">
+              Nullam port <span className="text-[#8BF645]">entesque</span>{" "}
+              ultricies
+            </span>
+            <br />
+            <span className="mb-[0.12em]">tempus lacus metus sit amet</span>
+          </h1>
+          <div className="block-hero_img_wrap absolute inset-0 w-full h-full">
+            <Image
+              fill
+              sizes="100vw"
+              priority
+              className="object-cover object-center"
+              src={"/background.png"}
+              alt={"hero image"}
+            />
+          </div>
+        </div>
+      </section>
       <section className="flex justify-center overflow-hidden p-[30vh_0_25vh]">
         <div className="relative block--title-xl">
           <blockquote className="block-quote-medium-text text-left b-quote-section relative z-[1]">
@@ -524,7 +672,7 @@ export const Footer = () => {
            </div>
            <div className="order-1 flex w-full lg:w-[32.15%] mb-8">
              <a>
-               <p className="font-bebas text-6xl">FVDLANCE <br/> CREATIVE</p>
+               <p className="font-bebas text-3xl">FVDLANCE <br/> CREATIVE</p>
              </a>
            </div>
         </div>
